39a45275ce1186e825564f6dcf23fb3d4bd8a8ae
Deo-Gracias David TONON <David15tonon@gmail.com>
2025-04-08 22:06:31 +0200

    Update tekbot_maze.launch.py

--- maze_solving/launch/tekbot_maze.launch.py
+++ maze_solving/launch/tekbot_maze.launch.py
@@ -1,4 +1,4 @@
-  GNU nano 8.1                                                              tekbot_maze.launch.py                                                                       
+                                                                    
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -31,7 +31,3 @@ def generate_launch_description():
         "launch",
         "empty_world.launch.py"],
     )
-
-
-^G Aide         ^O Écrire	^F Chercher     ^K Couper	^T Exécuter     ^C Emplacement  M-U Annuler     M-A Marquer     M-] -> Crochet  M-B Précédent
-^X Quitter	^R Lire fich.   ^\ Remplacer    ^U Coller	^J Justifier    ^/ Aller ligne  M-E Refaire     M-6 Copier	^B Retrouver    M-F Suivant
